f94e81305cab613ef79f69dae232894300891c54
olavarriaga <41701736+olavarriaga@users.noreply.github.com>
2024-11-13 17:29:51 -0300

    add license

--- update_timesheets_mdo/__manifest__.py
+++ update_timesheets_mdo/__manifest__.py
@@ -3,6 +3,8 @@
     'version': '1.0',
     'category': 'Accounting',
     'summary': 'Custom module to update analytic lines',
+    'license': 'LGPL-3',
+    'author': 'Newcom LCS',
     'description': 'Updates the general_account_id for analytic lines daily.',
     'depends': ['account'],
     'data': [
