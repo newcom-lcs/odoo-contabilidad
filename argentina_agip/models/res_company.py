41cbe4193cebba4dc7c080678b676e1aee8145ec
Manuel Nuñez <89217597+mnunez-newcom@users.noreply.github.com>
2025-03-18 14:13:29 +0000

    sobreescribiendo get_agip_data

---
+++ argentina_agip/models/res_company.py
@@ -0,0 +1,18 @@
+from odoo import models, fields, api, _
+
+# from pyafipws.padron import PadronAFIP
+from odoo.exceptions import UserError, RedirectWarning
+import logging
+import json
+import requests
+# from dateutil.relativedelta import relativedelta
+_logger = logging.getLogger(__name__)
+
+
+class ResCompany(models.Model):
+    _inherit = "res.company"
+
+    def get_agip_data(self, partner, date):
+        raise UserError(_(
+            'cosito del coso '
+            'AGIP'))
