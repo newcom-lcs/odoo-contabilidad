2308e59855fc94f6f5621161216e9d590eddc093
Manuel Olavarriaga <olavrriagam@gmail.com>
2025-05-27 22:15:43 -0300

    sales_order_fields in invoices

---
+++ related_sales_order_fields_invoices/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Related Sales Order Fields in Invoices',
+    'version': '16.0.1.0.0',
+    'summary': 'Adds related sales order fields to client invoice tree view',
+    'author': 'Your Company',
+    'category': 'Accounting',
+    'depends': ['account', 'sale_management'],
+    'data': [
+        'views/account_move_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+} 
---
+++ related_sales_order_fields_invoices/models/account_move.py
@@ -0,0 +1,47 @@
+from odoo import models, fields, api
+
+class AccountMove(models.Model):
+    _inherit = 'account.move'
+
+    so_tipo_negocio = fields.Char(
+        string='Tipo de Negocio',
+        compute='_compute_so_related_fields',
+        store=True,
+        search='_search_so_tipo_negocio',
+        groups='account.group_account_invoice'
+    )
+    so_business_unit_id = fields.Many2one(
+        'business.unit',
+        string='Business Unit',
+        compute='_compute_so_related_fields',
+        store=True,
+        search='_search_so_business_unit_id',
+        groups='account.group_account_invoice'
+    )
+
+    @api.depends('invoice_origin')
+    def _compute_so_related_fields(self):
+        SaleOrder = self.env['sale.order']
+        for move in self:
+            move.so_tipo_negocio = False
+            move.so_business_unit_id = False
+            if move.invoice_origin:
+                so = SaleOrder.search([('name', '=', move.invoice_origin)], limit=1)
+                if so:
+                    tipo_negocio = getattr(so, 'tipo_negocio', False)
+                    move.so_tipo_negocio = str(tipo_negocio).upper() if tipo_negocio else False
+                    move.so_business_unit_id = getattr(so, 'business_unit_id', False)
+
+    def _search_so_tipo_negocio(self, operator, value):
+        SaleOrder = self.env['sale.order']
+        sos = SaleOrder.search([('tipo_negocio', operator, value)])
+        if sos:
+            return [('invoice_origin', 'in', sos.mapped('name'))]
+        return [('id', '=', False)]
+
+    def _search_so_business_unit_id(self, operator, value):
+        SaleOrder = self.env['sale.order']
+        sos = SaleOrder.search([('business_unit_id', operator, value)])
+        if sos:
+            return [('invoice_origin', 'in', sos.mapped('name'))]
+        return [('id', '=', False)] 
