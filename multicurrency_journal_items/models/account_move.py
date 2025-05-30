e94d2aedafef55567191d55b9659a68f27a15d27
Manuel Olavarriaga <olavrriagam@gmail.com>
2025-05-30 12:36:06 -0300

    add supported for residual payments

--- multicurrency_journal_items/models/account_move.py
+++ multicurrency_journal_items/models/account_move.py
@@ -21,6 +21,12 @@ class AccountMove(models.Model):
         compute='_compute_amount_tax_second_currency',
         store=True
     )
+    amount_residual_signed_second_currency = fields.Monetary(
+        string='Residual Amount Signed (USD)',
+        currency_field='second_currency_id',
+        compute='_compute_amount_residual_signed_second_currency',
+        store=True
+    )
     second_currency_id = fields.Many2one(
         related='company_id.second_currency_id',
         store=True,
@@ -65,4 +71,17 @@ class AccountMove(models.Model):
                     move.date,
                 )
             else:
-                move.amount_tax_second_currency = 0.0 
+                move.amount_tax_second_currency = 0.0
+
+    @api.depends('amount_residual_signed', 'company_id', 'date', 'second_currency_id', 'line_ids.matched_debit_ids', 'line_ids.matched_credit_ids')
+    def _compute_amount_residual_signed_second_currency(self):
+        for move in self:
+            if move.second_currency_id:
+                move.amount_residual_signed_second_currency = move.company_currency_id._convert(
+                    move.amount_residual_signed,
+                    move.second_currency_id,
+                    move.company_id,
+                    move.date,
+                )
+            else:
+                move.amount_residual_signed_second_currency = 0.0 
