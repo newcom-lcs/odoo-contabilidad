a3d6a68f87fcb6a5a2bed5e464c9a3949f38479c
Gonzalo bolognese <gbolognese@newcom-lcs.com>
2025-07-03 15:07:59 -0300

    add model res company

---
+++ lock_date_ventas/wizard/account_change_lock_date.py
@@ -0,0 +1,16 @@
+from odoo import models, fields
+
+class AccountChangeLockDate(models.TransientModel):
+    _inherit = 'account.change.lock.date'
+
+    sales_lock_date = fields.Date(
+        string="Fecha de bloqueo para el diario ventas",
+        #default=lambda self: self.env.company.sales_lock_date,
+        help="Los usuarios no podrán modificar documentos de venta con fecha anterior o igual a esta fecha."
+    )
+    
+    def _prepare_lock_date_values(self):
+        values = super()._prepare_lock_date_values()
+        values['sales_lock_date'] = self.sales_lock_date
+        return values
+    
