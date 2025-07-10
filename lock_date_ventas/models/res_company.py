588abea78a2ac6e16d8b2aa00f0e99dc56a61c49
GonzaBolognese <gonza.1703@gmail.com>
2025-07-10 10:20:39 -0300

    update logica

--- lock_date_ventas/models/res_company.py
+++ lock_date_ventas/models/res_company.py
@@ -8,3 +8,7 @@ class ResCompany(models.Model):
         default=lambda self: self.env.company.sales_lock_date,
         help="Los usuarios no podrán modificar documentos de venta con fecha anterior o igual a esta fecha."
     )
+
+    def _get_user_sales_lock_date(self):
+        self.ensure_one()
+        return self.sales_lock_date
