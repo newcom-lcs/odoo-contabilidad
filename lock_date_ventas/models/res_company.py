ef3affed5a74f00237072f140ae0fe295071e759
Gonzalo bolognese <gbolognese@newcom-lcs.com>
2025-07-15 10:34:39 -0300

    Bloqueo funcional

--- lock_date_ventas/models/res_company.py
+++ lock_date_ventas/models/res_company.py
@@ -5,11 +5,9 @@ class ResCompany(models.Model):
 
     sales_lock_date = fields.Date(
         string="Fecha de bloqueo para el diario ventas",
-        tracking=True,
+        default=lambda self: self.env.company.sales_lock_date,
         help="Los usuarios no podrán modificar documentos de venta con fecha anterior o igual a esta fecha."
     )
 
     def _get_user_sales_lock_date(self):
-        """Devuelve la fecha de bloqueo de ventas para esta compañía."""
-        self.ensure_one()
         return self.sales_lock_date
--- lock_date_ventas/wizard/__init__.py
+++ lock_date_ventas/wizard/__init__.py
@@ -1,1 +1,2 @@
+# -*- coding: utf-8 -*-
 from . import account_change_lock_date
