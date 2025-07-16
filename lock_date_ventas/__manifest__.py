d386329621a0327f7f7967424e3fc7355a712d69
Gonzalo bolognese <gbolognese@newcom-lcs.com>
2025-07-16 16:07:23 -0300

    Se agrego carpeta static con el logo de newcom

--- lock_date_ventas/__manifest__.py
+++ lock_date_ventas/__manifest__.py
@@ -1,11 +1,11 @@
 {
-    'name': 'Daily sales lock date',
+    'name': 'Journal sales lock date',
     'version': '1.0',
     'category': 'Accounting',
     'summary': 'Custom module to update analytic lines',
     'license': 'LGPL-3',
     'author': 'Newcom LCS',
-    'description': 'Add a input for daily sales lock date',
+    'description': 'Add a input for Journal sales lock date',
     'depends': ['account', 'account_accountant'],
     'data': [
         'wizard/account_change_lock_date.xml',
